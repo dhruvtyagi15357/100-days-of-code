0b57d5274162329ef15bdeabc41ff637edf80ec8
dhruvtyagi15357 <65110515+dhruvtyagi15357@users.noreply.github.com>
2023-11-01 00:49:19 +0530

    Time: 45 ms (34.86%), Space: 13.5 MB (28.98%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -13,8 +13,7 @@ class Solution(object):
             return s
         
         rows = [[] for row in range(numRows)]
-
-        r,c = 0,0
+        r = 0
         flag = False # go down, flag = True # Go up
 
         for i in s:
@@ -23,37 +22,13 @@ class Solution(object):
             elif r == 0:
                 flag=False
 
+            self.append(rows, i, r)
             if not flag:
-                self.append(rows, i, r)
                 r+=1
             else:
-                self.append(rows, i, r)
                 r-=1
         res = ''
         for i in rows:
             for j in i:
                 res+=j
         return res
-
-        
-        # index = 0
-        # step = -1
-        # for char in s:
-        #     rows[index].append(char)
-        #     if index == 0:
-        #         step = 1
-        #     elif index == numRows - 1:
-        #         step = -1
-        #     index += step
-
-        # for i in range(numRows):
-        #     rows[i] = ''.join(rows[i])
-        # return ''.join(rows)
-        # i = 0
-        # flag = False    #go down, if flag == True, go up
-
-
-        # for char in s:
-        #     rows[i].append(char)
-
-        #     if 
