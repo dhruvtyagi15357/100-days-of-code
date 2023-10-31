f2b48c2e3076c27123b86e4ca6d9f6cd8226d545
dhruvtyagi15357 <65110515+dhruvtyagi15357@users.noreply.github.com>
2023-10-31 14:17:48 +0530

    Time: 47 ms (29.95%), Space: 13.4 MB (55.06%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -1,4 +1,7 @@
 class Solution(object):
+    def append(self, arr, element, row):
+        arr[row].append(element)
+
     def convert(self, s, numRows):
         """
         :type s: str
@@ -10,19 +13,42 @@ class Solution(object):
             return s
         
         rows = [[] for row in range(numRows)]
-        index = 0
-        step = -1
-        for char in s:
-            rows[index].append(char)
-            if index == 0:
-                step = 1
-            elif index == numRows - 1:
-                step = -1
-            index += step
 
-        for i in range(numRows):
-            rows[i] = ''.join(rows[i])
-        return ''.join(rows)
+        r,c = 0,0
+        flag = False # go down, flag = True # Go up
+
+        for i in s:
+            if r == numRows-1:
+                flag=True
+            elif r == 0:
+                flag=False
+
+            if not flag:
+                self.append(rows, i, r)
+                r+=1
+            else:
+                self.append(rows, i, r)
+                r-=1
+        res = ''
+        for i in rows:
+            for j in i:
+                res+=j
+        return res
+
+        
+        # index = 0
+        # step = -1
+        # for char in s:
+        #     rows[index].append(char)
+        #     if index == 0:
+        #         step = 1
+        #     elif index == numRows - 1:
+        #         step = -1
+        #     index += step
+
+        # for i in range(numRows):
+        #     rows[i] = ''.join(rows[i])
+        # return ''.join(rows)
         # i = 0
         # flag = False    #go down, if flag == True, go up
 
